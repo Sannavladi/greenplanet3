2c2a242f32bd9a745e432a82895f38c6d43a120e
Anna Shtanko <125276706+Sannavladi@users.noreply.github.com>
2023-04-03 12:37:34 +0200

    modified:   db.sqlite3 	modified:   pages/__pycache__/views.cpython-310.pyc 	modified:   pages/views.py 	modified:   static/css/chatbot.css 	new file:   static/images-general/icon.png 	new file:   static/scroll.js 	modified:   templates/index.html

--- pages/views.py
+++ pages/views.py
@@ -10,6 +10,8 @@ import ast
 import nltk
 from nltk.chat.util import Chat, reflections
 import unicodedata
+from datetime import datetime #for time Anna
+from django.utils.html import format_html
 
 
 
@@ -37,11 +39,35 @@ def contact (request) :
 
 @csrf_exempt
 def chatbot (request) :
-   data= {'page':'chatbot', 'history': []}
+   now = datetime.now()
+   data= {'page': 'chatbot', 'history': []} 
+   current_time = now.strftime("%H:%M:%S") # format d'heure à utiliser
+   
+   #current_date = now.strftime("%Y-%m-%d")
+   current_date = now.strftime("%d.%m.%Y")
+   
+   data['titre'] = current_date
+   
+   
+   msgBot = {"type" : "bot", "content": "Bonjour je suis Green Bot, comment puis-je vous aidez?", "time": current_time}
+   data['history'].append(msgBot) 
+
+#    Le code définit un dictionnaire de données appelé "data" avec deux clés:
+
+# "page": une chaîne de caractères qui indique que la page actuelle est la page du chatbot
+# "history": une liste vide qui stockera l'historique des messages échangés entre l'utilisateur et le chatbot.
    
    if request.method == 'POST':
+# Le code vérifie si la méthode de requête (request.method) est "POST". 
+
+# Si la méthode de requête est "POST", le code récupère le contenu de
+#  l'élément de formulaire nommé "history" en utilisant request.POST['history'] 
+# et l'assigne à une variable appelée "texthistory".
 
       texthistory = request.POST['history']
+      current_time = now.strftime("%H:%M:%S") # format d'heure à utiliser
+   
+   
       # si on a un historique dans le champs, il faut le récupérer au format json
       ##### penser à faire un "import json" en début de programme
       if (texthistory):
@@ -67,9 +93,15 @@ def chatbot (request) :
       reponse = chat.respond(texte_normalized)
      
       # on conserve les échange dans l'historique
-      msgUser = {"type" : "user", "content": message}
+      msgUser = {"type" : "user", "content": message, "time": current_time}
       data['history'].append(msgUser)
-      msgBot = {"type" : "bot", "content": reponse}
+      #msgBot = {"type" : "bot", "content": reponse, "time": current_time}
+      
+      if reponse:
+         msgBot = {"type" : "bot", "content": reponse, "time": current_time}
+      else:
+         msgBot = {"type": "bot", "content": format_html("Veuillez nous contacter à &nbsp;<a href='mailto:{}'> {} </a>", "contact@greenplanet.com", "contact@greenplanet.com"), "time": current_time}
+      
       data['history'].append(msgBot) 
  
 
---
+++ static/scroll.js
@@ -0,0 +1,11 @@
+<script>
+  var chatbox = document.getElementById("chatbox");
+  chatbox.scrollTop = chatbox.scrollHeight;
+</script>
+
+
+
+// Ce script définit la propriété scrollTop d'un élément HTML avec l'identifiant "chatbox" à sa scrollHeight,
+// permettant ainsi de faire défiler automatiquement l'élément jusqu'en bas.
+// Cela peut être utile dans une application de chat ou dans toute autre situation où vous souhaitez faire
+// défiler automatiquement vers le bas un élément conteneur qui affiche une conversation ou d'autres contenus.
